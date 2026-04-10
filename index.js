ab0ab658caacde2dd3762dcdd03902f1ceb0c08a
Tyler Long <aethdae@gmail.com>
2026-04-10 18:34:51 -0500

    logic for terminal commands added

---
+++ index.js
@@ -0,0 +1,54 @@
+import fs from "node:fs";
+
+const commands = ["a", "append", "r", "read"];
+const cliInput = process.argv.slice(2);
+
+cliInput.forEach((input, index) => {
+  if (commands.includes(input)) {
+    console.log(`Processing ${input}...`);
+    if ((input == "a") | (input == "append")) {
+      if (cliInput[index + 1]) {
+        if (!fs.existsSync("output.txt")) {
+          console.log("Creating file: 'output.txt'");
+        }
+        try {
+          fs.appendFile("output.txt", `\n${cliInput[index + 1]}`, (err) => {
+            if (err) {
+              throw new Error(err);
+            }
+            console.log(`Appended: ${cliInput[index + 1]} to output.txt`);
+          });
+        } catch (err) {
+          throw new Error(err);
+        }
+      } else {
+        console.log(`No argument provided for ${cliInput[index]}`);
+      }
+    }
+
+    if ((input == "r") | (input == "read")) {
+      if (cliInput[index + 1]) {
+        if (fs.existsSync(cliInput[index + 1])) {
+          try {
+            fs.readFile(cliInput[index + 1], "utf-8", (err, data) => {
+              if (err) {
+                throw new Error(err);
+              } else {
+                console.log(data);
+              }
+            });
+          } catch (err) {
+            throw new Error(err);
+          }
+        } else {
+          console.log(`File path doesn't exist at ${cliInput[index + 1]}`);
+        }
+      } else {
+        console.log(`No argument provided for ${cliInput[index]}`);
+      }
+    }
+  } else if (commands.includes(cliInput[index - 1])) {
+  } else {
+    console.log(`Incorrect arg at ${index}: ${cliInput[index]}`);
+  }
+});
